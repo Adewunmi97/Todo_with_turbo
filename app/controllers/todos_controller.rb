7b6a22e93301d9e54b4a5f3d512e7f87782c6b8c
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-05-23 12:15:26 +0100

    implement turbo stream actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,70 +1,52 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :set_todo, only: %i[show edit update destroy]
 
-  # GET /todos or /todos.json
-  def index
+def index
     @todos = Todo.all
   end
-
-  # GET /todos/1 or /todos/1.json
   def show
   end
-
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
-
-  # GET /todos/1/edit
   def edit
   end
-
-  # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
-
+  
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
+        format.turbo_stream # Renders create.turbo_stream.erb
+        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form", partial: "form", locals: { todo: @todo }) }
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
   end
-
-  # PATCH/PUT /todos/1 or /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: "Todo was successfully updated." }
-        format.json { render :show, status: :ok, location: @todo }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}", partial: "todo", locals: { todo: @todo }) }
+        format.html { redirect_to root_path, notice: "Todo updated successfully." }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
-
-  # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy!
-
+  
     respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}") }
       format.html { redirect_to todos_path, status: :see_other, notice: "Todo was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params.expect(:id))
-    end
-
-    # Only allow a list of trusted parameters through.
-    def todo_params
-      params.expect(todo: [ :title, :status ])
-    end
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+  def todo_params
+    params.require(:todo).permit(:title, :status)
+  end
 end
