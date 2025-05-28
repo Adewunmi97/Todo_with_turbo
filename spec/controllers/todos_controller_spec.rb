c32003f2e02c1d5f7703fd1f0534387167a7ca63
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-05-28 12:58:54 +0100

    add test for controller

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe TodosController, type: :controller do
+  let(:todo) { Todo.create!(title: 'Some Task', status: 'incomplete') }
+
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'returns a success response' do
+      get :edit, params: { id: todo.id }
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'updates the requested todo' do
+      patch :update, params: { id: todo.id, todo: { title: 'Updated Task' } }
+      todo.reload
+      expect(todo.title).to eq('Updated Task')
+    end
+  end
+
+  describe 'DELETE #destroy' do
+  let!(:todo) { Todo.create!(title: 'Destroy Me', status: 'incomplete') }
+
+  it 'destroys the requested todo' do
+    expect {
+      delete :destroy, params: { id: todo.id }, format: :turbo_stream
+    }.to change(Todo, :count).by(-1)
+  end
+end
+end
